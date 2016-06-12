fb8ddd1ad93acd9ab05867a53a7a9d9823c4fda4
Joris Wijlens <joris@smartworkx.nl>
2016-06-12 23:41:49 +0200

    Added vagrant  and ansible scripts for deployment

---
+++ scripts/package.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+var config = require('./../webpack.config.js');
+
+config.plugins.push(
+    new webpack.DefinePlugin({
+        "process.env": {
+            NODE_ENV: JSON.stringify("production")
+        }
+    })
+);
+
+webpack(config).run(function(err, stats) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Successfully packed");
+    }
+});
