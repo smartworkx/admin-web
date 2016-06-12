fc9ddf7e860307d59e7b207ecc0de54a6a82a7ae
Joris Wijlens <joris@smartworkx.nl>
2016-06-12 15:03:31 +0200

    Added first set up for testing with mocha

---
+++ test/vatReport.spec.js
@@ -0,0 +1,11 @@
+import expect from 'expect'
+import counter from '../src/reducers'
+
+describe('reducers', () => {
+    describe('counter', () => {
+
+        it('should ignore unknown actions', () => {
+            expect(1).toBe(1)
+        })
+    })
+})
