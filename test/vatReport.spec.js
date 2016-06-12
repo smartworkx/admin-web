6e0aa4ed9bc4ef677e9410e58ea43f4fbf1f373f
Joris Wijlens <joris@smartworkx.nl>
2016-06-12 15:59:32 +0200

    Test for jenkins

--- test/vatReport.spec.js
+++ test/vatReport.spec.js
@@ -7,5 +7,9 @@ describe('reducers', () => {
         it('should ignore unknown actions', () => {
             expect(1).toBe(1)
         })
+
+        it('should ignore unknown actions 2', () => {
+            expect(1).toBe(1)
+        })
     })
 })
