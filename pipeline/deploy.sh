6e9206ded5285de7849175eae459c0181dafb247
Joris Wijlens <joris@smartworkx.nl>
2017-08-07 20:43:21 +0200

    Added kubernetes file and implemented deploy.sh

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 echo deploy
+gcloud auth activate-service-account --key-file /tmp/service-account.json
+gcloud config set project smartworkx-173909
+kubectl apply -f sources/kubernetes/admin-web.yml
