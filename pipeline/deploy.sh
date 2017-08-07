ab56ca05f10e95ab1e4c01298acbb05a39b2ea68
Joris Wijlens <joris@smartworkx.nl>
2017-08-07 22:10:21 +0200

    Deploying admin-web to smartworkx cluster

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -2,4 +2,5 @@
 echo deploy
 gcloud auth activate-service-account --key-file /tmp/service-account.json
 gcloud config set project smartworkx-173909
+gcloud container clusters get-credentials --zone europe-west1-c smartworkx-cluster
 kubectl apply -f sources/kubernetes/admin-web.yml
