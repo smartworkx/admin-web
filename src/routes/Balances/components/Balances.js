18c5c8239b316069097e4e0d0b476bc796df3da4
Joris Wijlens <joris@smartworkx.nl>
2017-10-23 00:40:55 +0200

    Added fetch for balances page

--- src/routes/Balances/components/Balances.js
+++ src/routes/Balances/components/Balances.js
@@ -1,31 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router'
-import { Table, TableCell, TableHead, TableRow } from 'react-toolbox'
+import React, {Component} from 'react'
+import {Link} from 'react-router'
+import {Table, TableCell, TableHead, TableRow} from 'react-toolbox'
 import BalanceCreationForm from './BalanceCreationForm'
 
-const Balances = (props) => {
-  return <div>
-    <h1>Balances</h1>
-    <BalanceCreationForm />
-    <Table selectable={false}>
-      <TableHead>
-        <TableCell>Date</TableCell>
-        <TableCell>Creation date</TableCell>
-        <TableCell>Description</TableCell>
-        <TableCell />
-      </TableHead>
-      {props.data.map(item => {
-        return (
-          <TableRow key={item.id}>
-            <TableCell>{item.date}</TableCell>
-            <TableCell>{item.creationDateTime}</TableCell>
-            <TableCell>{item.description}</TableCell>
-            <TableCell><Link to={'/balances/' + item.id} >Details</Link></TableCell>
-          </TableRow>
-        )
-      })}
-    </Table>
-  </div>
+class Balances extends Component {
+  constructor(props) {
+    super(props)
+    props.fetch(props.id)
+  }
+
+  render() {
+
+    return <div>
+      <h1>Balances</h1>
+      <BalanceCreationForm />
+      <Table selectable={false}>
+        <TableHead>
+          <TableCell>Date</TableCell>
+          <TableCell>Creation date</TableCell>
+          <TableCell>Description</TableCell>
+          <TableCell />
+        </TableHead>
+        {this.props.data.map(item => {
+          return (
+            <TableRow key={item.id}>
+              <TableCell>{item.date}</TableCell>
+              <TableCell>{item.creationDateTime}</TableCell>
+              <TableCell>{item.description}</TableCell>
+              <TableCell><Link to={'/balances/' + item.id}>Details</Link></TableCell>
+            </TableRow>
+          )
+        })}
+      </Table>
+    </div>
+  }
 }
 
 export default Balances
